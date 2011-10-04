22ef2f57dacddb0fa4829b14dffca3179c144fcc
santiavenda <santiavenda2@gmail.com>
2011-10-04 00:53:07 -0300

    agrego un main

--- pomodoropy/pomodoropy.py
+++ pomodoropy/pomodoropy.py
@@ -1,8 +1,18 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-#!/usr/bin/env python2.6
-__author__="santiago"
-__date__ ="$15/09/2011 23:16:56$"
+#!/usr/bin/env python2.7
+__author__ = "santiago"
+__date__ = "$15/09/2011 23:16:56$"
+
+from core import ControladorPomodoro
+
+
+def main():
+    print "Main Pomodoro Timer"
+    controladorPomodoro = ControladorPomodoro()
+    controladorPomodoro.iniciar_pomodoro()
+    for i in range(16):
+        controladorPomodoro.avanzar_estado()
 
 if __name__ == "__main__":
-    print "Hello World"
+    main()
+
+
